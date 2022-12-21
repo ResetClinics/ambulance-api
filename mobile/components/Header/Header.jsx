bccdcce6a74f369af5dbf3ce61bfd2dbc80cf4ea
Любовь Макарова <arsenii07@gmail.com>
2022-12-21 21:13:59 +0300

    How Images insert in project

--- mobile/components/Header/Header.jsx
+++ mobile/components/Header/Header.jsx
@@ -1,16 +1,12 @@
 import { View, Image, Linking, Text, TouchableHighlight, StyleSheet } from "react-native";
 import React from 'react'
-const src = "./../../../assets/favicon.png"
 
 export const Header = () => {
   return (
     <View style={styles.root}>
       <Image
-        source={{uri: "https://hi-tech.mail.ru/news/58746-beskonechnaya-kartinka-zavirusilas-v-seti/imageset/2300004/"}}
-        style={{
-          width: 50,
-          height: 50
-        }}
+        style={styles.img}
+        source={require('./favicon.png')}
       />
       <TouchableHighlight onPress={() => Linking.openURL("https://google.com")}>
         <Text>1212</Text>
@@ -22,5 +18,9 @@ export const Header = () => {
 const styles = StyleSheet.create({
   root: {
     backgroundColor: 'red', height: 100
+  },
+  img: {
+    width: 48,
+    height: 48
   }
 })
