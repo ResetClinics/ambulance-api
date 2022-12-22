224168a8316bdf8119b976382c3d7a9652ce41a9
Любовь Макарова <arsenii07@gmail.com>
2022-12-22 16:24:32 +0300

    img

--- mobile/src/components/Menu/Menu.jsx
+++ mobile/src/components/Menu/Menu.jsx
@@ -3,8 +3,13 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Brigade } from "../Brigade";
 import { CurrentCall } from "../CurrentCall";
+import {COLORS} from "../../../constants";
+import { Image } from "react-native";
 
 const Tab = createBottomTabNavigator();
+
+const img1 = '../../../assets/menu/img1.png'
+const img3 = '../../../assets/menu/img5.png'
 
 export const Menu = () => {
   return (
@@ -14,28 +19,45 @@ export const Menu = () => {
           let iconName;
 
           if (route.name === 'Бригада') {
-            iconName = focused
-              ? 'ios-information-circle'
-              : 'ios-information-circle-outline';
+            iconName = focused ? img1 : img1;
           } else if (route.name === 'Текущий вызов') {
-            iconName = focused ? 'ios-list' : 'ios-list-outline';
+            iconName = focused ? img1 : img1;
+          } else if (route.name === 'История вызовов') {
+            iconName = focused ? img1 : img1;
+          } else if (route.name === 'Чат') {
+            iconName = focused ? img1 : img1;
+          } else if (route.name === 'Уведомления') {
+            iconName = focused ? img1 : img1;
+          } else if (route.name === 'Профиль') {
+            iconName = focused ? img1 : img1;
           }
 
           // You can return any component that you like here!
-          return <Ionicons name={iconName} size={size} color={color} />;
+          return <Image source={require(img3)} size={size} color={color} style={styles.img}/>
         },
-        tabBarActiveTintColor: 'tomato',
-        tabBarInactiveTintColor: 'gray',
+        tabBarActiveTintColor: COLORS.primary,
+        tabBarInactiveTintColor: COLORS.gray,
       })}
     >
-      <Tab.Screen name="Бригада" component={Brigade} options={{ tabBarBadge: 3 }} style={styles.root}/>
-      <Tab.Screen name="Текущий вызов" component={CurrentCall} />
+      <Tab.Screen name="Бригада" component={Brigade} style={styles.tab} />
+      <Tab.Screen name="Текущий вызов" component={CurrentCall} style={styles.tab} />
+      <Tab.Screen name="История вызовов" component={CurrentCall} style={styles.tab} />
+      <Tab.Screen name="Чат" component={CurrentCall} style={styles.tab} />
+      <Tab.Screen name="Уведомления" component={CurrentCall} style={styles.tab} options={{ tabBarBadge: 3 }}/>
+      <Tab.Screen name="Профиль" component={CurrentCall} style={styles.tab} />
     </Tab.Navigator>
   );
 }
 
 const styles = {
-  root: {
-    color: 'red'
+  tab: {
+    display: 'inline-block',
+    fontSize: 25,
+    color: COLORS.gray,
+  },
+  img: {
+    width: 24,
+    height: 24
   }
 }
+
