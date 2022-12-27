415a8e48e98acfde54b4a4a13505ddd97c3baef6
Любовь Макарова <arsenii07@gmail.com>
2022-12-27 17:35:01 +0300

    png to webp

---
+++ mobile/src/components/CardItem/CardItem.jsx
@@ -0,0 +1,122 @@
+import React, { useState } from "react";
+import { Button, Card, Paragraph, Title } from "react-native-paper";
+import { Image, Text, View } from "react-native";
+import { COLORS, SIZES } from "../../../constants";
+
+export  const CardItem = ({ address, subject, date, time, comment, goToMapPage, status, children } ) => {
+  const [active, setActive] = useState(false)
+
+  const onDetailedClick = () => {
+    setActive(!active)
+  }
+
+  const BtnChange = () => {
+    if(active) {
+      return <Button style={styles.btn} onPress={onDetailedClick}>Свернуть</Button>
+    } else {
+      return <Button onPress={onDetailedClick}>Подробнее</Button>
+    }
+  }
+
+  return (
+    <Card style={styles.root} children>
+      <Card.Content>
+        <Text style={styles.status}>{status}</Text>
+        <Title>{address}</Title>
+        <Paragraph>{subject}</Paragraph>
+        <View style={styles.date}>
+          <Text style={styles.text}>
+            Дата:
+            {' '}
+            {date}
+            {' '}
+            г.
+          </Text>
+          <Text style={styles.text}>
+            Время заказа:
+            {' '}
+            {time}
+          </Text>
+        </View>
+        <View style={active ? styles.active : styles.content}>
+          <Text style={styles.info}>Коментарий к вызову:</Text>
+          <Text style={styles.info}>{comment}</Text>
+          <View style={styles.wrap}>
+            <Button
+              onPress={() => goToMapPage()}
+              style={styles.btn} icon={() => (
+              <Image
+                source={require('../../../assets/map_marker.png')}
+                style={{ width: 17, height: 23 }}
+              />
+            )}>Посмотреть карту</Button>
+            <Button icon={() => (
+              <Image
+                source={require('../../../assets/close.png')}
+                style={{ width: 24, height: 24 }}
+              />
+            )}>Отменить вызов</Button>
+          </View>
+        </View>
+      </Card.Content>
+      <Card.Actions style={styles.actions}>
+        {BtnChange()}
+        {children}
+      </Card.Actions>
+    </Card>
+  )
+}
+
+const styles = {
+  root: {
+    backgroundColor: COLORS.white,
+    borderRadius: 4,
+    borderColor: COLORS.primary,
+    borderWidth: 1,
+    marginBottom: 32
+  },
+  date: {
+    flexDirection: 'row',
+    marginTop: 16,
+    marginBottom: 8
+  },
+  status: {
+    letterSpacing: 0.4,
+    lineHeight: 16,
+    fontSize: SIZES.fs16,
+    color: COLORS.gray,
+  },
+  text: {
+    color: COLORS.gray,
+    fontSize: SIZES.fs12,
+    letterSpacing: 0.4,
+    lineHeight: 16,
+    width: '45%',
+  },
+  actions: {
+    marginBottom: -27,
+    right: 5
+  },
+  info: {
+    fontSize: SIZES.fs16,
+    color: COLORS.black,
+    letterSpacing: 0.4,
+    lineHeight: 16,
+    marginTop: 16
+  },
+  wrap: {
+    alignItems: 'flex-start',
+    marginLeft: -10,
+    marginTop: 24,
+    marginBottom: 10
+  },
+  btn: {
+    marginBottom: 5
+  },
+  content: {
+    display: 'none'
+  },
+  active: {
+    display: 'flex'
+  }
+}
--- mobile/src/screens/CallHistory/CallHistory.jsx
+++ mobile/src/screens/CallHistory/CallHistory.jsx
@@ -1,10 +1,53 @@
-import { Text, View } from "react-native";
 import React from "react";
+import { Layout } from "../../shared";
+import { FlatList } from "react-native";
+import { CardItem } from "../../components";
 
-export const CallHistory = () => {
+const data = [
+  {
+    address: 'Пресненская наб., 2 (этаж 1)',
+    subject: 'Вызов врача-нарколога',
+    date: '12.12.2022',
+    speciality: 'Психиатор- нарколог',
+    time: '12:45',
+    comment: 'Мужчина ,  43 года нужна детоксикация организма , возмоно психотерапевтическая помощь',
+    status: 'Активный вызов'
+  },
+  {
+    address: 'Пресненская наб., 2 (этаж 1)',
+    subject: 'Вызов врача-нарколога',
+    date: '12.12.2022',
+    time: '12:45',
+    comment: 'Мужчина ,  43 года нужна детоксикация организма , возмоно психотерапевтическая помощь, нужна детоксикация' +
+      ' организма , возмоно психотерапевтическая помощь',
+    status: 'Вызов завершен'
+  },
+  {
+    address: 'Пресненская набережная, 2 (этаж 1), кв. 589',
+    subject: 'Вызов врача-нарколога',
+    date: '12.12.2022',
+    time: '12:45',
+    comment: 'Мужчина ,  43 года нужна детоксикация организма , возмоно психотерапевтическая помощь, нужна детоксикация' +
+      ' организма , возмоно психотерапевтическая помощь',
+    status: 'Вызов завершен'
+  },
+]
+
+export const CallHistory = ({navigation}) => {
+  const goToMapPage = () => {
+    navigation.navigate('Маршрут', {
+      screen: 'Home',
+      params: {
+        screen: 'Маршрут',
+      },
+    });
+  }
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'blue', }}>
-      <Text>Home!</Text>
-    </View>
-  );
+    <Layout>
+      <FlatList
+        data={data}
+        renderItem={({item}) => <CardItem {...item} goToMapPage={goToMapPage} />}
+      />
+    </Layout>
+  )
 }
