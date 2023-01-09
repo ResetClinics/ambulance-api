d354b36faa1402724ff0e9c128a344aec5704eea
Любовь Макарова <arsenii07@gmail.com>
2023-01-09 11:49:15 +0300

    index.js App move

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -4,12 +4,12 @@ import React, { useCallback, useEffect, useState } from 'react'
 import { MagicModalPortal } from 'react-native-magic-modal'
 import * as Font from 'expo-font'
 import * as SplashScreen from 'expo-splash-screen'
-import { expo } from './app.json'
-import italicFont from './assets/fonts/Roboto-Italic.ttf'
-import boldFont from './assets/fonts/Roboto-Bold.ttf'
-import regularFont from './assets/fonts/Roboto-Regular.ttf'
-import mediumFont from './assets/fonts/Roboto-Medium.ttf'
-import { Routes } from './src/navigation/Routes'
+import { expo } from '../app.json'
+import italicFont from '../assets/fonts/Roboto-Italic.ttf'
+import boldFont from '../assets/fonts/Roboto-Bold.ttf'
+import regularFont from '../assets/fonts/Roboto-Regular.ttf'
+import mediumFont from '../assets/fonts/Roboto-Medium.ttf'
+import { Routes } from './navigation/Routes'
 
 const App = () => {
   const [appIsReady, setAppIsReady] = useState(false)
