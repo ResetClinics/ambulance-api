ce07557fdd3998a07e7f32e45b83063328b6e459
alergy <evtin@yandex.ru>
2023-01-10 22:28:52 +0300

    Deploy configs

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,17 +1,4 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-const { getDefaultConfig } = require('metro-config')
+const { getDefaultConfig } = require('expo/metro-config')
 
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig()
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
-    },
-    resolver: {
-      assetExts: assetExts.filter((ext) => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg'],
-    },
-  }
-})()
+module.exports = (async () => getDefaultConfig(__dirname))()
