dd12d1d419fab818ba9f6c980050dc01267e56ef
Любовь Макарова <arsenii07@gmail.com>
2023-01-11 11:13:23 +0300

    fix label in team

--- mobile/src/components/TeamList/TeamList.jsx
+++ mobile/src/components/TeamList/TeamList.jsx
@@ -1,7 +1,10 @@
-import { FlatList, StyleSheet, View } from 'react-native'
+import {
+  FlatList, Image, StyleSheet, View
+} from 'react-native'
 import React from 'react'
 import { Card, Title, Paragraph } from 'react-native-paper'
 import { COLORS, FONTS } from '../../../constants'
+import labelImg from '../../../assets/images/label.png'
 
 const data = [
   {
@@ -33,14 +36,16 @@ const data = [
 const CardItem = (item) => {
   const { name, speciality } = item
   return (
-    <View style={styles.wrapper}>
-      <Card style={styles.root}>
-        <Card.Content>
-          <Title style={styles.title}>{name}</Title>
-          <Paragraph style={styles.subtitle}>{speciality}</Paragraph>
-        </Card.Content>
-      </Card>
-    </View>
+    <Card style={styles.root}>
+      <Card.Content style={styles.wrap}>
+        <Title style={styles.title}>{name}</Title>
+        <Paragraph style={styles.subtitle}>{speciality}</Paragraph>
+      </Card.Content>
+      <Image
+        source={labelImg}
+        style={styles.label}
+      />
+    </Card>
   )
 }
 
@@ -64,16 +69,25 @@ const styles = StyleSheet.create({
     borderRadius: 4,
     borderColor: COLORS.primary,
     borderWidth: 1,
-    marginBottom: 32
-  },
-  wrapper: {
+    marginBottom: 32,
     position: 'relative',
+
+  },
+  label: {
+    position: 'absolute',
+    width: 46,
+    height: 46,
+    right: 10,
+    bottom: -23
   },
   title: {
     ...FONTS.title,
   },
   subtitle: {
     ...FONTS.text,
-    marginTop: 16
+    marginTop: 16,
   },
+  wrap: {
+    padding: 16,
+  }
 })
