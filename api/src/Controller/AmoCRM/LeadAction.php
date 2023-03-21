84ce99e4c5bf6121b626c1e263fcd4b173e5e4aa
alergy <evtin@yandex.ru>
2023-03-21 11:27:04 +0300

    AmoCRM hook

---
+++ api/src/Controller/AmoCRM/LeadAction.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\AmoCRM;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/api/amo-crm/lead', name: 'amo-crm_lead', methods: ["POST"])]
+class LeadAction extends AbstractController
+{
+    public function __invoke(Request $request): JsonResponse
+    {
+        $data = $request->getContent();
+        file_put_contents(dirname(__DIR__) .'/../../var/test.txt', print_r($data, true), FILE_APPEND);
+        return $this->json($data, Response::HTTP_OK);
+    }
+}
