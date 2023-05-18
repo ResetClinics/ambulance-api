15879b64c73dc7412bfd98b2d782e3d698d14470
alergy <evtin@yandex.ru>
2023-05-19 00:00:18 +0300

    dev

---
+++ api/src/Services/CallingSender.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Services;
+
+use App\Entity\Calling\Calling;
+use App\Repository\DeviceRepository;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class CallingSender
+{
+    private HttpClientInterface $client;
+    private DeviceRepository $devices;
+
+    public function __construct(
+        HttpClientInterface $client,
+        DeviceRepository $devices
+    )
+    {
+        $this->client = $client;
+        $this->devices = $devices;
+    }
+
+    public function sendToAdmin(Calling $calling): void
+    {
+        $devices = $this->devices->findBy(['user' => $calling->getAdmin()]);
+
+        foreach ($devices as $device){
+            $this->client->request('POST', 'https://exp.host/--/api/v2/push/send', [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Accept' => 'application/json',
+                ],
+                'json' => [
+                    "data" =>  [
+                        "callingId" =>  $calling->getId(),
+                        "callingStatus" =>  $calling->getStatus(),
+                    ],
+                    'to' => $device->getId(),
+                    'title' => 'Внимание новый заказ',
+                    'body' =>  $calling->getAddress(),
+                    'subtitle' => 'asfasdfasdf',
+                ],
+            ]);
+        }
+    }
+
+}
