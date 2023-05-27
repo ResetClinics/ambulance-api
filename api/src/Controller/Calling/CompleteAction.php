cf23f3a202a32b120d58c419560f9a33e4c7c61d
alergy <evtin@yandex.ru>
2023-05-27 10:55:04 +0300

    dev

--- api/src/Controller/Calling/CompleteAction.php
+++ api/src/Controller/Calling/CompleteAction.php
@@ -5,14 +5,19 @@ declare(strict_types=1);
 namespace App\Controller\Calling;
 
 use AmoCRM\Client\AmoCRMApiClient;
+use AmoCRM\Collections\NotesCollection;
+use AmoCRM\Exceptions\AmoCRMApiException;
 use AmoCRM\Filters\LeadsFilter;
+use AmoCRM\Helpers\EntityTypesInterface;
 use AmoCRM\Models\LeadModel;
+use AmoCRM\Models\NoteType\ServiceMessageNote;
 use App\Entity\Calling\Calling;
 use App\Flusher;
 use App\Repository\CallingRepository;
 use App\Services\AmoCRM;
 use App\Services\CallingSender;
 use DateTimeImmutable;
+use DateTimeZone;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -40,12 +45,40 @@ class CompleteAction extends AbstractController
 
         $leads = $this->client->leads()->get($filter);
 
+        $message = 'Информация от бригады' . PHP_EOL;
+        $message .= 'Заявка №' . $calling->getNumberCalling() . PHP_EOL;
+        $message .= $calling->getPrice() ? 'Итоговая цена ' . $calling->getPrice() . PHP_EOL : '';
+        $message .= $calling->getName() ? 'ФИО пациента ' . $calling->getName() . PHP_EOL : '';
+        $message .= $calling->getAge() ? 'Возраст пациента ' . $calling->getAge() . PHP_EOL : '';
+        $message .= $calling->getNote() ? 'Примечание ' . $calling->getNote() . PHP_EOL : '';
+
+        $currentDate = new DateTimeImmutable('now', new DateTimeZone('Europe/Moscow'));
+
+        $entityId = null;
         /** @var LeadModel $lead */
         foreach ($leads as $lead) {
+            $entityId = $lead->getId();
             $lead->setStatusId(45084664);
+            $lead->setName($currentDate->format('d:m:y') . ' ' . $calling->getName());
+            $lead->setPrice($calling->getPrice());
         }
 
         $this->client->leads()->update($leads);
+
+        $notesCollection = new NotesCollection();
+        $serviceMessageNote = new ServiceMessageNote();
+        $serviceMessageNote->setEntityId($entityId)
+            ->setText($message)
+            ->setService('Выездное приложение')
+            ->setCreatedBy(0);
+
+        $notesCollection->add($serviceMessageNote);
+
+        try {
+            $leadNotesService = $this->client->notes(EntityTypesInterface::LEADS);
+            $leadNotesService->add($notesCollection);
+        } catch (AmoCRMApiException $e) {
+        }
 
         $calling->setComplete(new DateTimeImmutable());
         $flusher->flush();
