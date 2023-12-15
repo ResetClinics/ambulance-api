9f6e3edc5f457a45d976778d1cc85287f016b89f
evtin <evtin@ya.ru>
2023-12-15 12:18:09 +0300

    Delete Call entity

---
+++ api/src/Entity/MedTeam/MedTeam.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace App\Entity\MedTeam;
+
+use ApiPlatform\Doctrine\Common\Filter\DateFilterInterface;
+use ApiPlatform\Doctrine\Orm\Filter\DateFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
+use ApiPlatform\Metadata\ApiResource;
+use App\Entity\User\User;
+use App\Repository\MedTeam\MedTeamRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: MedTeamRepository::class)]
+#[ApiResource(
+    normalizationContext: ['groups' => ['med-team:read']],
+    denormalizationContext: ['groups' => ['med-tam:write']],
+    paginationClientEnabled: true,
+    paginationClientItemsPerPage: true
+)]
+#[ApiFilter(
+    DateFilter::class,
+    properties: [
+        'plannedA' => DateFilterInterface::EXCLUDE_NULL,
+        'startedAt' => DateFilterInterface::EXCLUDE_NULL,
+        'completedAt' => DateFilterInterface::EXCLUDE_NULL,
+    ]
+)]
+class MedTeam
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    #[Groups(['med-tam:read'])]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    #[Groups(['med-tam:read', 'med-tam:write'])]
+    #[Assert\NotNull]
+    private ?DateTimeImmutable $plannedAt = null;
+
+    #[ORM\Column(nullable: true)]
+    #[Groups(['med-tam:read', 'med-tam:write'])]
+    private ?DateTimeImmutable $startedAt = null;
+
+    #[ORM\Column(nullable: true)]
+    #[Groups(['med-tam:read', 'med-tam:write'])]
+    private ?DateTimeImmutable $completedAt = null;
+
+    #[ORM\Column(length: 32)]
+    #[Groups(['med-tam:read', 'med-tam:write'])]
+    #[Assert\Choice(choices: [
+        'draft',
+        'scheduled',
+        'work',
+        'completed',
+        'cancelled',
+    ])]
+    #[ApiFilter(SearchFilter::class, properties: ['status' => 'exact'])]
+    private string $status = 'draft';
+
+    #[ORM\ManyToOne]
+    #[Groups(['med-tam:read', 'med-tam:write'])]
+    #[ApiFilter(SearchFilter::class, properties: ['admin.id' => 'exact'])]
+    private ?User $admin = null;
+
+    #[ORM\ManyToOne]
+    #[Groups(['med-tam:read', 'med-tam:write'])]
+    #[ApiFilter(SearchFilter::class, properties: ['admin.id' => 'exact'])]
+    private ?User $doctor = null;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPlannedAt(): ?DateTimeImmutable
+    {
+        return $this->plannedAt;
+    }
+
+    public function setPlannedAt(DateTimeImmutable $plannedAt): self
+    {
+        $this->plannedAt = $plannedAt;
+
+        return $this;
+    }
+
+    public function getStartedAt(): ?DateTimeImmutable
+    {
+        return $this->startedAt;
+    }
+
+    public function setStartedAt(?DateTimeImmutable $startedAt): self
+    {
+        $this->startedAt = $startedAt;
+
+        return $this;
+    }
+
+    public function getCompletedAt(): ?DateTimeImmutable
+    {
+        return $this->completedAt;
+    }
+
+    public function setCompletedAt(?DateTimeImmutable $completedAt): self
+    {
+        $this->completedAt = $completedAt;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getAdmin(): ?User
+    {
+        return $this->admin;
+    }
+
+    public function setAdmin(?User $admin): self
+    {
+        $this->admin = $admin;
+
+        return $this;
+    }
+
+    public function getDoctor(): ?User
+    {
+        return $this->doctor;
+    }
+
+    public function setDoctor(?User $doctor): self
+    {
+        $this->doctor = $doctor;
+
+        return $this;
+    }
+
+}
---
+++ api/src/Repository/MedTeam/MedTeamRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repository\MedTeam;
+
+use App\Entity\MedTeam\MedTeam;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<MedTeam>
+ *
+ * @method MedTeam|null find($id, $lockMode = null, $lockVersion = null)
+ * @method MedTeam|null findOneBy(array $criteria, array $orderBy = null)
+ * @method MedTeam[]    findAll()
+ * @method MedTeam[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class MedTeamRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, MedTeam::class);
+    }
+
+    public function save(MedTeam $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(MedTeam $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+}
