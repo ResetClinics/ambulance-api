7549d359a7705b83df82cd3d9ce0ffadbfa193b6
evtin <evtin@ya.ru>
2023-12-27 10:54:39 +0300

    ˚

--- api/src/Command/InitAgreementCommand.php
+++ api/src/Command/InitAgreementCommand.php
@@ -2,10 +2,12 @@
 
 namespace App\Command;
 
+use App\Entity\Partner;
 use App\Flusher;
 use App\Repository\Partner\Agreement\AgreementRepository;
 use App\Repository\Partner\Agreement\AgreementTemplateRepository;
 use App\Repository\Partner\Agreement\RowRepository;
+use App\Repository\PartnerRepository;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -24,6 +26,7 @@ class InitAgreementCommand extends Command
         private readonly RowRepository $agreementRows,
         private readonly AgreementRepository $agreements,
         private readonly AgreementTemplateRepository $templates,
+        private readonly PartnerRepository $partners,
         private readonly Flusher $flusher,
     )
     {
@@ -44,11 +47,31 @@ class InitAgreementCommand extends Command
 
         $this->flusher->flush();
 
-
-
+        foreach ($this->partners as $partner){
+            $this->createAgreement($partner);
+        }
 
         $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
 
         return Command::SUCCESS;
+    }
+
+    private function createAgreement(Partner $partner)
+    {
+        $agreement = new Partner\Agreement\Agreement();
+        $agreement->setPartner($partner);
+        $agreement->setStartsAt(new \DateTimeImmutable('01.12.2023'));
+        /** @var Partner\Agreement\AgreementTemplate $template */
+        foreach ($this->templates as $template){
+            $row = new Partner\Agreement\Row();
+            $row->setAgreement($agreement);
+            $row->setService($template->getService());
+            $row->setDistance($template->getDistance());
+            $row->setPercent($template->getPercent());
+            $row->setRepeatNumber($template->getRepeatNumber());
+
+            $agreement->addRow($row);
+        }
+        $this->agreements->save($agreement, true);
     }
 }
