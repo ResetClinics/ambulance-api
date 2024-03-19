d2bb26e3123dd7de8455271b94c68a316e41acff
evtin <evtin@ya.ru>
2024-03-19 13:07:40 +0300

    MedTeam added type

--- api/src/Entity/Hospital/Clinic.php
+++ api/src/Entity/Hospital/Clinic.php
@@ -22,11 +22,11 @@ class Clinic
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
-    #[Groups(['clinic:read', 'clinic:write'])]
+    #[Groups(['clinic:read', 'clinic:write', 'hospital:read'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(['clinic:read', 'clinic:write'])]
+    #[Groups(['clinic:read', 'clinic:write', 'hospital:read'])]
     private ?string $name = null;
 
     #[ORM\OneToMany(mappedBy: 'clinic', targetEntity: Hospital::class)]
