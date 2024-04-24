3fe8f10f72b1d5232c4b8795caf8472bfd29965a
evtin <evtin@ya.ru>
2024-04-24 12:40:05 +0300

    Отчет по пользователям

--- api/src/Query/User/UsersReport/Fetcher.php
+++ api/src/Query/User/UsersReport/Fetcher.php
@@ -53,6 +53,7 @@ class Fetcher
             $result[$user['id']] = [
                 'id' => $user['id'],
                 'name' => $user['name'],
+                'roles' => $user['roles'],
                 'calls' => 0,
                 'amount' => 0,
                 'averageCheck' => 0,
