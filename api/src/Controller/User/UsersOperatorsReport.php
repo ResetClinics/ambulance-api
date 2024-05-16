9191128f222e983588e029d2462aea635e3b7b3b
evtin <evtin@ya.ru>
2024-05-16 21:56:50 +0300

    Награда операторов

---
+++ api/src/Controller/User/UsersOperatorsReport.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller\User;
+use App\Query\User\UsersReport\Fetcher;
+use App\Query\User\UsersReport\Query;
+use Exception;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
+
+class UsersOperatorsReport extends AbstractController
+{
+    public function __construct(
+        private readonly DenormalizerInterface $denormalizer,
+        private readonly Fetcher $fetcher,
+    )
+    {
+    }
+
+    /**
+     * @throws Exception
+     * @throws ExceptionInterface
+     */
+    #[Route(path: '/api/users/report', name: 'users_report', methods: 'GET', priority: 10)]
+    public function __invoke(Request $request): JsonResponse
+    {
+        $query = $this->denormalizer->denormalize($request->query->all(), Query::class);
+
+        $result = $this->fetcher->fetch($query);
+
+        return $this->json(array_values($result));
+    }
+}
---
+++ api/src/Query/User/UsersOperatorReport/Query.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Query\User\UsersOperatorReport;
+
+class Query
+{
+    public ?string $period = null;
+    public ?string $sort = 'name';
+    public ?string $order = 'asc';
+}
