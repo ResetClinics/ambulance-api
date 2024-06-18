9c8c29b398be30fb0cf2b040f7c0270ded24c7cb
evtin <evtin@ya.ru>
2024-06-18 15:07:55 +0300

    Отчет администратора

--- api/src/Controller/AmoCRM/TestAction.php
+++ api/src/Controller/AmoCRM/TestAction.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller\AmoCRM;
 
+use AmoCRM\Client\AmoCRMApiClient;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,31 +18,25 @@ class TestAction extends AbstractController
 
     public function __invoke(Request $request)
     {
-      // $apiClient = new \AmoCRM\Client\AmoCRMApiClient(
-      //     'd80b0f1f-1687-4b1e-8abd-9f3cbbe7a19e',
-      //     'fCUzh7hiQ1bcuKQSrdJVp7Mnwnwi4b2vsK4W7yzhBCcumEkvRcHl3wX3hVxglhmK',
-      //     'https://ambulance.rc-respect.ru/api/amo-crm/auth/callback'
-      // );
-      // $fff = $apiClient->getOAuthClient()->getOAuthButton(
-      //     [
-      //         'title' => 'Установить интеграцию',
-      //         'compact' => true,
-      //         'class_name' => 'className',
-      //         'color' => 'default',
-      //         'error_callback' => 'handleOauthError',
-      //     ]
-      // );
+       $apiClient = new AmoCRMApiClient(
+           'd80b0f1f-1687-4b1e-8abd-9f3cbbe7a19e',
+           'fCUzh7hiQ1bcuKQSrdJVp7Mnwnwi4b2vsK4W7yzhBCcumEkvRcHl3wX3hVxglhmK',
+           'https://reset-med.ru/app/api/amo-crm/auth/callback'
+       );
+       $fff = $apiClient->getOAuthClient()->getOAuthButton(
+           [
+               'title' => 'Установить интеграцию',
+               'compact' => true,
+               'class_name' => 'className',
+               'color' => 'default',
+               'error_callback' => 'handleOauthError',
+           ]
+       );
 
-      // echo $fff;
+       echo $fff;
 
 
 
-      // dd($fff);
-
-        $filename = '1.png';
-        $imageFile = '/app/uploads/' . $filename;
-
-        return $this->file($imageFile, $filename, ResponseHeaderBag::DISPOSITION_INLINE);
 
         //$token = new \League\OAuth2\Client\Token\AccessToken();
 
