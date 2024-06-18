28a0610d0eb91e9f81cd5cdf55821baa956608ea
evtin <evtin@ya.ru>
2024-06-18 11:04:11 +0300

    Отчет администратора

--- api/src/Controller/ImageAction.php
+++ api/src/Controller/ImageAction.php
@@ -5,24 +5,22 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/api/uploads/{filename}', name: 'amo-crm_test', methods: ["GET"])]
 class ImageAction extends AbstractController
 {
-
-    public function __invoke(string $filename, Request $request,KernelInterface $kernel): BinaryFileResponse
+    public function __invoke(string $filename, Request $request, KernelInterface $kernel): Response
     {
         $projectDir = $kernel->getProjectDir();
+        $imageFile = $projectDir . '/uploads/' . $filename;
+        $type = pathinfo($imageFile, PATHINFO_EXTENSION);
+        $data = file_get_contents($imageFile);
+        $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
 
-        $imageFile = $projectDir .'/uploads/' . $filename;
-
-        return $this->file($imageFile, $filename, ResponseHeaderBag::DISPOSITION_INLINE);
+        return $this->json($base64);
     }
 }
