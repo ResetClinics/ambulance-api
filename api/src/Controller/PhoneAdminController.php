cc63ea5bf118bf528553d2ed5f7e5e4732b82850
evtin <evtin@ya.ru>
2024-10-04 16:19:24 +0300

    XML для импорта телефонов администраторов

---
+++ api/src/Controller/PhoneAdminController.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Entity\User\User;
+use App\Repository\UserRepository;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PhoneAdminController extends AbstractController
+{
+
+    public function __construct(
+       private readonly SerializerInterface $serializer,
+        private readonly UserRepository $users,
+    )
+    {
+    }
+
+    #[Route('/api/phone/admin', name: 'version')]
+    public function phoneAdmin(): Response
+    {
+        $users = $this->users->findAllByPermission('can_be-admin');
+
+        $data = [];
+        /** @var User $user */
+        foreach ($users as $user) {
+            $data[] = [
+                'Name' => $user->getName(),
+                'Telephone' => '+' . $user->getPhone(),
+            ];
+        }
+
+        $xml = $this->serializer->serialize($data, 'xml');
+
+        $response = new Response($xml);
+        $response->headers->set('Content-Type', 'application/xml');
+
+        return $response;
+    }
+}
