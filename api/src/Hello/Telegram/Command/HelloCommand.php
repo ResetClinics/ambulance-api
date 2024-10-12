d9326c168bc5d90659b6fd8fea405ff1d2b2c882
evtin <evtin@ya.ru>
2024-10-12 11:46:04 +0300

    Added Telegram bundle

--- api/src/Hello/Telegram/Command/HelloCommand.php
+++ api/src/Hello/Telegram/Command/HelloCommand.php
@@ -6,7 +6,6 @@ namespace App\Hello\Telegram\Command;
 use BoShurik\TelegramBotBundle\Telegram\Command\AbstractCommand;
 use BoShurik\TelegramBotBundle\Telegram\Command\PublicCommandInterface;
 use TelegramBot\Api\BotApi;
-use TelegramBot\Api\Types\Inline\InlineKeyboardMarkup;
 use TelegramBot\Api\Types\ReplyKeyboardMarkup;
 use TelegramBot\Api\Types\Update;
 
@@ -31,7 +30,8 @@ class HelloCommand extends AbstractCommand implements PublicCommandInterface
 
         $buttons[] = [
             'text' => 'Контакт',
-            'request_contact' => true
+            'request_contact' => true,
+            'callback_data' => 'contact'
         ];
 
         $text = sprintf('Hello *%s*', $who);
@@ -43,5 +43,22 @@ class HelloCommand extends AbstractCommand implements PublicCommandInterface
             null,
             new ReplyKeyboardMarkup([$buttons])
         );
+    }
+
+
+    public function handleCallbackQuery(BotApi $api, Update $update): void
+    {
+        $callbackQuery = $update->getCallbackQuery();
+        if ($callbackQuery->getData() === 'contact') {
+            $chatId = $callbackQuery->getMessage()->getChat()->getId();
+            $api->sendContact($chatId, 'John Doe', '+1234567890');
+        }
+    }
+
+    protected function configureCallbackQueryHandlers(BotApi $api): array
+    {
+        return [
+            'contact' => [$this, 'handleCallbackQuery'],
+        ];
     }
 }
