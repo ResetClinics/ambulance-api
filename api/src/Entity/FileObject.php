7493825311a4b0a558e560e2b11c36a50e73f096
evtin <evtin@ya.ru>
2024-11-20 14:08:15 +0300

    Удаление файлов

--- api/src/Entity/FileObject.php
+++ api/src/Entity/FileObject.php
@@ -3,6 +3,7 @@ namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Post;
@@ -41,7 +42,8 @@ use Vich\UploaderBundle\Mapping\Annotation as Vich;
                     ])
                 )
             )
-        )
+        ),
+        new Delete(),
     ],
     outputFormats: [
         'json' => ['application/json'],
