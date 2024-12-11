333919f329ded303bbbab1e18b43bb0127c583ff
evtin <evtin@ya.ru>
2024-12-11 15:12:47 +0300

    Фильтр пользователей по городам и ролям

--- api/src/Controller/DeviceController.php
+++ api/src/Controller/DeviceController.php
@@ -7,34 +7,37 @@ namespace App\Controller;
 use App\Entity\Device;
 use App\Flusher;
 use App\Repository\DeviceRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\NonUniqueResultException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DeviceController extends AbstractController
 {
-
-
-    private DeviceRepository $devices;
-    private Flusher $flusher;
-
-    public function __construct(DeviceRepository $devices, Flusher $flusher)
+    public function __construct(
+        private readonly UserRepository $users,
+        private readonly DeviceRepository $devices,
+        private readonly Flusher $flusher
+    )
     {
-        $this->devices = $devices;
-        $this->flusher = $flusher;
     }
 
+    /**
+     * @throws NonUniqueResultException
+     */
     #[Route('/api/devices/{id}', name: 'device_create_or_update', methods: ['POST'])]
     public function createOrUpdate(string $id): Response
     {
-
         $device = $this->devices->find($id);
 
+        $user = $this->users->findOneByPhone($this->getUser()->getUserIdentifier());
+
         if (!$device) {
-            $device = new Device($id, $this->getUser());
+            $device = new Device($id, $user);
             $this->devices->save($device);
         }else{
-            $device->setUser($this->getUser());
+            $device->setUser($user);
         }
 
         $this->flusher->flush();
