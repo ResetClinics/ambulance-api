a25b41166e138a2ae6b51c51a00319ab3ac80331
evtin <evtin@ya.ru>
2025-01-12 06:54:16 +0300

    Фильтр вызовов по услугам

--- api/src/Entity/Calling/Calling.php
+++ api/src/Entity/Calling/Calling.php
@@ -292,7 +292,6 @@ use Gedmo\Mapping\Annotation as Gedmo;
     SearchByFieldsFilter::class,
     properties: ['search']
 )]
-#[ApiFilter(SearchFilter::class, properties: ['services.service.id' => 'exact'])]
 class Calling
 {
     #[ORM\Id]
