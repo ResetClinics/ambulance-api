6abf32ed591dfb21e0144a0f6c2dbc466dfe9527
Dmitriy Evtin <evtin@yandex.ru>
2025-02-24 13:35:57 +0300

    Disable api doc in prod

--- api/src/Controller/HomeController.php
+++ api/src/Controller/HomeController.php
@@ -28,9 +28,9 @@ class HomeController extends AbstractController
         ]);
     }
 
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->json(['app' => 'ambulance']);
+        return $this->json(null);
     }
 }
