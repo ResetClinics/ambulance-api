c3ae1a0348d683c17817f5b9246e8463d09df77b
Dmitriy Evtin <evtin@yandex.ru>
2025-02-25 19:50:26 +0300

    В лк сотрудника обрезаем адрес на 6 символов

--- api/src/Controller/My/CallsAction.php
+++ api/src/Controller/My/CallsAction.php
@@ -69,7 +69,7 @@ class CallsAction extends AbstractController
                     'name' => $call->getDoctor()->getName(),
                 ] : null,
                 'callId' => $call->getId(),
-                'name' => substr($call->getAddress(), 0, -6) . '...',
+                'name' => mb_substr($call->getAddress(), 0, -6) . '...',
                 'amount' => $call->getPrice(),
                 'reward' => 0,
                 'subRows' => [],
