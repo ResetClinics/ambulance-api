fdc9a9ee20fb79d4704e0653a389fd35ca5d9d1a
Dmitriy Evtin <evtin@yandex.ru>
2025-12-23 16:40:53 +0300

    dev

--- api/src/Controller/Partner/CallsAction.php
+++ api/src/Controller/Partner/CallsAction.php
@@ -36,6 +36,7 @@ class CallsAction extends AbstractController
             'partner' => $id,
             'page' => $request->query->getInt('page', 1),
             'perPage' => $request->query->getInt('per_page', 20),
+            'amounts' => true
         ];
         
         // Добавляем остальные параметры из запроса, исключая пустые значения и уже обработанные
